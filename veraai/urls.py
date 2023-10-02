37834d486a6b51ace2f8f4196b6f15fec335a847
lilcoded7@gmil.com <@the,the,the123454321>
2023-10-02 18:10:01 -0400

    updated

--- veraai/urls.py
+++ veraai/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('connect/bms/api/', views.VINSINCBUILDINGDATACreateUpdateView.as_view(), name='bms-data-api'),#1
     path('bms/api/v1/', views.BuildingAPIVIew.as_view(), name='register-building'),#2
-    path('upload/file/data/', views.UploadBuildingDataCreateUpdateView.as_view(), name='upload-file-data-api')
-
-    
+    path('upload/file/data/', views.UploadBuildingDataCreateUpdateView.as_view(), name='upload-file-data-api'),
+    path('lighting/optimization/', views.plot_energy_data)
 
 ]
--- veraai/views.py
+++ veraai/views.py
@@ -43,5 +43,77 @@ class UploadBuildingDataCreateUpdateView(CreateAPIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from io import BytesIO
+import base64
+import pandas as pd
+import matplotlib.pyplot as plt
+from django.conf import settings
+import os
+
+def optimize_lighting(df):
+    # Replace this with your actual optimization algorithm
+    # Example: Reduce lighting by 20% during non-working hours
+    working_hours = (8, 18)  # Assuming 8 AM to 6 PM are working hours
+    df['Lighting(W/m2)'] = df.apply(lambda row: row['Lighting(W/m2)'] * 0.8 if not working_hours[0] <= row['Date/Time'].hour < working_hours[1] else row['Lighting(W/m2)'], axis=1)
+    return df
+
+def plot_energy_data(request):
+    current_user = request.user
+    user_buildings = Building.objects.filter(user=current_user)
+    
+    if not user_buildings.exists():
+        return render(request, 'no-building-found.html')
+    
+    df = pd.DataFrame()
+    
+    for user_building in user_buildings:
+        upload_data = UploadBuildingData.objects.filter(building=user_building).first()
+        if upload_data and upload_data.upload_file:
+            file_path = upload_data.upload_file.path
+            building_df = pd.read_excel(file_path)
+        else:
+            energy_data = VINSINCBUILDINGDATA.objects.filter(building=user_building)
+            if energy_data:
+                building_df = pd.DataFrame(list(energy_data.values()))
+            else:
+                building_df = pd.DataFrame()
+        
+        df = pd.concat([df, building_df], ignore_index=True)
+    
+    relevant_columns = [
+        "Date/Time",
+        "Lighting(W/m2)",
+        "General Lighting(W/m2)",
+        "Radiant Temperature(°C)",
+        "Air Temperature(°C)",
+    ]
+    
+    if not df.empty:
+        df = df[relevant_columns]
+        df = optimize_lighting(df)
+        
+        plt.figure(figsize=(12, 6))
+        for column in relevant_columns[1:]:
+            plt.plot(df["Date/Time"], df[column], label=column)
+        plt.xlabel("Date/Time")
+        plt.ylabel("Values")
+        plt.title("Energy Consumption Data with Lighting Optimization")
+        plt.legend()
+
+        buffer = BytesIO()
+        plt.savefig(buffer, format="png")
+        plt.close()
+        buffer.seek(0)
+        image_data = buffer.getvalue()
+        base64_image = base64.b64encode(image_data).decode("utf-8")
+        context = {"image": base64_image}
+        
+        return render(request, "code.html", context)
+    else:
+        return render(request, 'no-data-found.html')
+
+
 
 
